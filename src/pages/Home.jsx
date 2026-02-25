9daac04915f4349b3695dabfd1aa8444bb098273
MiyuRuu22 <wpmswijesinghe@gmail.com>
2026-02-25 09:30:48 +0530

    Install: Vercel Analytics

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import Calculator from "../components/Calculator";
 import DonationButton from "../components/DonationButton";
 import Layout from "../components/Layout";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
